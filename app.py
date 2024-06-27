e1ab278ae9bfe56ff98882c3fdafc3f4e8b3cd06
dexterholmes <119703753+dexterholmes@users.noreply.github.com>
2024-06-27 12:42:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ import streamlit as st
 
 # Add custom CSS to hide the GitHub icon
 hide_github_icon = """
+<style>
 #GithubIcon {
-  visibility: hidden;
+    visibility: hidden;
 }
+</style>
 """
 st.markdown(hide_github_icon, unsafe_allow_html=True)
 
-# test
+# Your app code goes here
+
